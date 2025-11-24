d23dde7cab82f46b7edada11fa1a3485aba16cc5
akib1902 <ahmed22205101902@diu.edu.bd>
2025-11-24 19:21:18 +0600

    Update 88. Merge Sorted Array.py

--- Leetcode problem solving/88. Merge Sorted Array.py
+++ Leetcode problem solving/88. Merge Sorted Array.py
@@ -7,7 +7,7 @@ class Solution:
         p2=n-1
         p3=m+n-1
         while p2 >= 0:
-            if p1>0 and nums1[p1] > nums2[p2]:
+            if p1>=0 and nums1[p1] > nums2[p2]:
                 nums1[p3] = nums1[p1]
                 p3-=1
                 p1-=1
