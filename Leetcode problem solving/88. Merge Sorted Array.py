4c2c59a5c256d3d8e5ea49c3d3d670ec818b2411
akib1902 <ahmed22205101902@diu.edu.bd>
2025-11-24 19:01:08 +0600

    Create 88. Merge Sorted Array.py

---
+++ Leetcode problem solving/88. Merge Sorted Array.py
@@ -0,0 +1,19 @@
+# check and compare every element linearly in both array.
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        p1=m-1
+        p2=n-1
+        p3=m+n-1
+        while p2 >= 0:
+            if p1>0 and nums1[p1] > nums2[p2]:
+                nums1[p3] = nums1[p1]
+                p3-=1
+                p1-=1
+            else:
+                nums1[p3] = nums2[p2]
+                p2-=1
+                p3-=1
+
+#using while and draw picture/visualization to solve
