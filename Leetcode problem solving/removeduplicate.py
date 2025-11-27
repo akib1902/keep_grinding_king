fb96f25c1aab1cfd7173b11550faed0065d2a02a
akib1902 <ahmed22205101902@diu.edu.bd>
2025-11-27 14:08:11 -0600

    learned two pointers
    > picturing the perfect solution is what need

--- Leetcode problem solving/removeduplicate.py
+++ Leetcode problem solving/removeduplicate.py
@@ -1,13 +1,11 @@
-from typing import List
-
+# if duplicate front if not replace with prev 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) <= 2:
-            return len(nums)
-        
-        k = 2
-        for i in range(2, len(nums)):
-            if nums[i] != nums[k-2]:
-                nums[k] = nums[i]
-                k += 1
+        k=1
+        if len(nums)<=1:
+            return k
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[i-k]:
+                nums[k]=nums[i]
+                k+=1
         return k
